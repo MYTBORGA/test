30910fc72cf8fd2a29fbf8dc3c1b4cd5b3653e4d
MYTBORGA <15jn0134@JN-PC22.jnsv01.jec.ac.jp>
2015-07-15 15:46:20 +0900

    first

---
+++ kadai14E.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Gaku_inf
+{
+	char g_no[7];
+	int g_kamoku1;
+	int g_kamoku2;
+	double g_heikin;
+	struct Gaku_inf *next;
+}dat;
+
+int main(void)
+{
+	char no[7];
+	dat *g_pstart,*g_padd;
+	
+	g_padd = (dat *)malloc(sizeof(dat));
+	g_padd->next = NULL;
+	
+	g_pstart = g_padd;
+	
+	printf("ŠwÐ”Ô†-->");
+	while(scanf("%s",g_padd->g_no) != EOF)
+	{
+		printf("‰È–Ú1-->");
+		scanf("%d",&g_padd->g_kamoku1);
+		
+		printf("‰È–Ú2-->");
+		scanf("%d",&g_padd->g_kamoku2);
+		
+		g_padd->g_heikin = (double)(g_padd->g_kamoku1 + g_padd->g_kamoku2) / 2;
+		
+		g_padd->next = (dat *)malloc(sizeof(dat));
+		g_padd = g_padd->next;
+		
+		printf("ŠwÐ”Ô†-->");
+	}
+	
+	g_padd->next = NULL;
+	
+	printf("\n");
+	
+	g_padd = g_pstart;
+	
+	 while(g_padd->next != NULL)
+	 {
+	 	printf("ŠwÐ”Ô†:%s\n",g_padd->g_no);
+		
+		printf("‰È–Ú1:%d\n",g_padd->g_kamoku1);
+		
+		printf("‰È–Ú2:%d\n",g_padd->g_kamoku2);
+		
+		printf("•½‹Ï:%.1f\n",g_padd->g_heikin);
+		
+		g_padd = g_padd->next;
+	 }
+	 
+	 return 0;
+} 
